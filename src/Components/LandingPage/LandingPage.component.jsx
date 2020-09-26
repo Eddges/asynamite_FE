4377ceb1e8eb317c85e7cc4cdc2dccf3518e3d83
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-27 00:14:44 +0530

    landingpage component

---
+++ src/Components/LandingPage/LandingPage.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './LandingPage.styles.module.css';
+
+function LandingPage() {
+    return (
+        <div className = {styles.parent}>
+            <div className = {styles.left_content}>
+                
+            </div>
+            <div className = {styles.right_conent}>
+
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
