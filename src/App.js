d871e841704c233c829ea6ce3d3642c179e27029
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-27 16:16:34 +0530

    loginpage minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LandingPage from './Components/LandingPage/LandingPage.component';
+import SignInPage from './Components/SignInPage/signin.component';
 
 
 function App() {
   return (
     <React.Fragment>
     
-        <LandingPage/>
+        {/* <LandingPage/> */}
+        <SignInPage/>
       
     </React.Fragment>
   );
