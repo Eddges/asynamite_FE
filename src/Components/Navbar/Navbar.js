ad692c43ce43e9ed5b685c410e41a4fd8ad1e316
Eddges <shivamshekhar.i7@gmail.com>
2020-09-30 21:11:52 +0530

    Navbar and Layout reponsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -35,6 +35,28 @@ class Navbar extends React.Component{
                         <img src={Result} alt="Result" />
                     </NavLink>
                 </div>
+                <div className={classes.MenuMobile}>
+                    <NavLink to="/" exact className={classes.MenuItem} activeClassName={classes.Active}>
+                        <span>HOME</span>
+                        <img src={HomeLogo} alt="Home" />
+                    </NavLink>
+                    <NavLink to="/contests" className={classes.MenuItem} activeClassName={classes.Active}>
+                        <span>CONTESTS</span>
+                        <img src={Trophy} alt="Contests" className={classes.ResultsIcon} />
+                    </NavLink>
+                    <NavLink to="/practice" className={classes.MenuItem} activeClassName={classes.Active}>
+                        <span>PRACTICE</span>
+                        <img src={Command} alt="Practice" />
+                    </NavLink>
+                    <NavLink to="/dashboard" className={classes.MenuItem} activeClassName={classes.Active}>
+                        <span>DASHBOARD</span>
+                        <img src={Dashboard} alt="Dashboard" />
+                    </NavLink>
+                    <NavLink to="/results" className={classes.MenuItem} activeClassName={classes.Active}>
+                        <span>RESULTS</span>
+                        <img src={Result} alt="Result" />
+                    </NavLink>
+                </div>
                 <div className={classes.ButtonDiv}>
                     <NavLink to = '/login'><button className={classes.LoginButton}>Login</button></NavLink> 
                     <NavLink to = '/register'><button className={classes.SignUpButton}>Sign Up</button></NavLink> 
