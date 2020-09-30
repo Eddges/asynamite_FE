99d0b4001ee4a67a1941d966916a4672e2fa8e76
nagendra0032 <nagendrakumar0032@gmail.com>
2020-09-30 19:33:16 +0530

    result page done

--- src/Components/Result/result.component.jsx
+++ src/Components/Result/result.component.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import styles from './results.module.css';
 import Layout from '../Layout/Layout';
+import resultCard from '../../assets/resultcard.png';
 
 
 function Results() {
     return (
         <Layout>
-            <div className={styles.header_section}></div>
+            <div className = {styles.parent}>
+                <div className = {styles.header_container}>
+                    <p className = {styles.heading}> Recent Contests</p>
+                </div>
+
+                <div className = {styles.result_container}>
+                    <img src= {resultCard} alt=""/>
+                    
+                    
+                    <div className = {styles.details}>
+                        <p className = {styles.detail_heading}>HACKATHON_4</p>
+                        <p className = {styles.date_title}> Announced On</p>
+                        <div className = {styles.date}>
+                            <p>August 20, 2020</p>
+                        </div>
+                    </div>
+
+
+                    <div className = {styles.view}>
+                        <button>View Results</button>
+                    </div>
+                </div>
+            </div>
         </Layout>
         
     )
