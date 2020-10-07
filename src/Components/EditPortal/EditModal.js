f82d75ddd3facd3c230b015771273fcab5451e0d
nagendra0032 <nagendrakumar0032@gmail.com>
2020-10-08 01:06:19 +0530

    some css changes in editModal

--- src/Components/EditPortal/EditModal.js
+++ src/Components/EditPortal/EditModal.js
@@ -16,16 +16,22 @@ function EditModal({ open ,onClose}) {
             </div>
 
             <div className = {styles.information_wrapper}>
-              <form>
-                <input type = "text" placeholder = "_name"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_dob"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_college"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_branch"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_graduation_year"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_phone"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_resume"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_github_link"  className = {styles.input_in}  />
-                <input type = "text" placeholder = "_linkedIn_link"  className = {styles.input_in}  />
+              <form >
+                <div className = {styles.input_wrapper}>
+                  <input type = "text" placeholder = "_name"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_dob"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_college"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_branch"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_graduation_year"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_phone"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_resume"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_github_link"  className = {styles.input_in}  />
+                  <input type = "text" placeholder = "_linkedIn_link"  className = {styles.input_in}  />
+                </div>
+                <div className = {styles.btn}>
+                  <button>Submit</button>
+                </div>
+                
 
               </form>
             </div>
